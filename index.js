1bf9d7d277ec6394b6185b0dc03d913fd036852c
Danny <lannyducas@gmail.com>
2022-07-20 10:29:47 -0400

    commit tree fbb2f5d84573e1b19ea21354960e3a44225c3702

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+/*const returnFirstTwoDrivers = (function(['Antonia', 'Nuru', 'Amari', 'Mo']){
+    const newArray = [...returnFirstTwoDrivers];
+    
+}) */
+
+const returnFirstTwoDrivers = function(array){
+    return array.slice(0,2);
+    
+};
+
+const returnLastTwoDrivers = function(array){
+    return array.slice(array.length -2 ,array.length);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = function (multiplyValue){
+    return function(fare){
+        return multiplyValue * fare;
+    }
+}
+
+//createFareMultiplier(multiplyValue)(fare);
+
+//so what I want is to create a function that takes a value (4) 
+//and returns a function that takes another value (fare) and calculates the two
+//advantage is mutability, I can change either function seperately
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(array, fn){
+    return fn(array);
+}
